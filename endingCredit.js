44838f3d6a17c6fd0e884ea43d3245244239278e
전하은 <hani@jeonha-eun-ui-MacBookPro.local>
2022-09-26 17:01:22 +0900

    Add: css file 수정

--- endingCredit.js
+++ endingCredit.js
@@ -1,4 +1,3 @@
-//별 찍기
 let stars = 100;
 
 for (let i = 0; i <= stars; i++) {
@@ -24,3 +23,26 @@ function getStarPosition() {
 
   return { x: randomX, y: randowY };
 }
+
+function textToSpeech() {
+  if (
+    typeof SpeechSynthesisUtterance === "undefined" ||
+    typeof window.speechSynthesis === "undefined"
+  ) {
+    alert("이 브라우저는 음성 합성을 지원하지 않습니다.");
+    return;
+  }
+  window.speechSynthesis.cancel();
+
+  const text = document.getElementsByClassName("thanks")[0].innerText;
+  const speechMsg = new SpeechSynthesisUtterance();
+  speechMsg.rate = 1;
+  speechMsg.pitch = 1;
+  speechMsg.text = text;
+
+  window.speechSynthesis.speak(speechMsg);
+}
+setTimeout(textToSpeech, 18000);
+
+const play = document.getElementsByClassName("audio")[0];
+// setTimeout(() => play.play(), 1000);
